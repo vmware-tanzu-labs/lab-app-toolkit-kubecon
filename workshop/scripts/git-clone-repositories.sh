91b7e2f55c22d542dd77963fbc46324c263d62e8
Cora Iberkleid <ciberkleid@gmail.com>
2022-05-14 00:02:27 -0400

    delete unused files

--- workshop/scripts/git-clone-repositories.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-# Clone sample application to session git server
-cd /opt/git/repositories
-git clone --bare https://github.com/ciberkleid/hello-go.git
-
--- workshop/setup.d/setup.sh
+++ workshop/setup.d/setup.sh
@@ -19,20 +19,17 @@ if [[ $(which tanzu) == "" ]]; then
   ./install.sh
 fi
 
-# Clone repositories
-/opt/workshop/scripts/git-clone-repositories.sh &
-
 # Install TCE package repository
 tanzu package repository add tce-repo --url projects.registry.vmware.com/tce/main:$TCE_VERSION --namespace tanzu-package-repo-global
 
 # Install Application Toolkit
-envsubst < /opt/workshop/setup.conf/app-toolkit-values-template.yaml > /opt/workshop/setup.conf/app-toolkit-values.yaml
+envsubst < /opt/workshop/setup.d/app-toolkit-values-template.yaml > /opt/workshop/setup.d/app-toolkit-values.yaml
 tanzu secret registry add registry-credentials --server $REGISTRY_HOST --username $REGISTRY_USERNAME --password $REGISTRY_PASSWORD --export-to-all-namespaces --yes
 APP_TOOLKIT_VERSION=$(tanzu package available list app-toolkit.community.tanzu.vmware.com -o json | jq -r ".[].version" | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n 1)
-tanzu package install app-toolkit --package-name app-toolkit.community.tanzu.vmware.com --version $APP_TOOLKIT_VERSION -f /opt/workshop/setup.conf/app-toolkit-values.yaml --namespace tanzu-package-repo-global
+tanzu package install app-toolkit --package-name app-toolkit.community.tanzu.vmware.com --version $APP_TOOLKIT_VERSION -f /opt/workshop/setup.d/app-toolkit-values.yaml --namespace tanzu-package-repo-global
 
-kp secret create git-credentials --git-url $GIT_PROTOCOL://$GIT_HOST --git-user $GIT_USERNAME || true
-
-# Run scripts in the background (so the workshop UI can load now)
-#/opt/workshop/scripts/kpack-create-builder.sh &
-#/opt/workshop/scripts/carto-create-supply-chain.sh &
+## Clone sample application to session git server
+## (Can do this earlier in this script, in the background using a separate script)
+#cd /opt/git/repositories
+#git clone --bare https://github.com/ciberkleid/hello-go.git
+#kp secret create git-credentials --git-url $GIT_PROTOCOL://$GIT_HOST --git-user $GIT_USERNAME || true
